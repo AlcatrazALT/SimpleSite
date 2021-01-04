4b32d7ed9857b76c49ea8f82e32a491dad0811c1
AlcatrazALT <olegmazalevsky21@yandex.ru>
2021-01-04 16:14:02 +0700

    feat: Add DataManager

---
+++ SimpleSite/Domain/DataManager.cs
@@ -0,0 +1,16 @@
+﻿using SimpleSite.Domain.Repositories.Abstract;
+
+namespace SimpleSite.Domain
+{
+    public class DataManager
+    {
+        public ITextFieldsRepository TextFields { get; set; }
+        public IServiceItemsRepository ServiceItems { get; set; }
+
+        public DataManager(ITextFieldsRepository textFields, IServiceItemsRepository serviceItems)
+        {
+            TextFields = textFields;
+            ServiceItems = serviceItems;
+        }
+    }
+}
