515f94aa8161aad3c0eab2209d71883d51b4ba75
AlcatrazALT <olegmazalevsky21@yandex.ru>
2021-01-06 17:04:53 +0700

    feat: Add ServiceItemsController

---
+++ SimpleSite/Areas/Admin/Controllers/ServiceItemsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SimpleSite.Domain;
+using SimpleSite.Domain.Entities;
+using SimpleSite.Service;
+using System;
+using System.IO;
+
+namespace SimpleSite.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ServiceItemsController : Controller
+    {
+        private readonly DataManager dataManager;
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public ServiceItemsController(DataManager dataManager, IWebHostEnvironment webHostEnvironment)
+        {
+            this.dataManager = dataManager;
+            this.webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Edit(Guid id)
+        {
+            var service = id == default ? new ServiceItem() : dataManager.ServiceItems.GetServiceItemById(id);
+            return View(service);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ServiceItem serviceItem, IFormFile titleImageFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if (titleImageFile != null)
+                {
+                    serviceItem.TitleImagePath = titleImageFile.FileName;
+                    using var stream = new FileStream(
+                        Path.Combine(webHostEnvironment.WebRootPath, "images/", titleImageFile.FileName),
+                        FileMode.Create);
+
+                    titleImageFile.CopyTo(stream);
+                }
+                dataManager.ServiceItems.SaveServiceItem(serviceItem);
+                return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+            }
+            return View(serviceItem);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Guid id)
+        {
+            dataManager.ServiceItems.DeleteServiceItem(id);
+            return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+        }
+    }
+}
