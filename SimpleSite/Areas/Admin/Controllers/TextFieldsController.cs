cfce221fcdb67556b16a4f53c0ded2c0cc37dac2
AlcatrazALT <olegmazalevsky21@yandex.ru>
2021-01-06 16:44:56 +0700

    feat: Add TextFieldsController

---
+++ SimpleSite/Areas/Admin/Controllers/TextFieldsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleSite.Domain;
+using SimpleSite.Domain.Entities;
+using SimpleSite.Service;
+
+namespace SimpleSite.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TextFieldsController : Controller
+    {
+        private readonly DataManager dataManager;
+
+        public TextFieldsController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public IActionResult Edit(string codeWord)
+        {
+            var textField = dataManager.TextFields.GetTextFieldByCodeWord(codeWord);
+            return View(textField);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TextField textField)
+        {
+            if (ModelState.IsValid)
+            {
+                dataManager.TextFields.SaveTextField(textField);
+                return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+            }
+            return View(textField);
+        }
+    }
+}
