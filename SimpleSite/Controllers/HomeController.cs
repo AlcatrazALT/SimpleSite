7222de73b0245430b8bb0a151790bf6debc758eb
AlcatrazALT <olegmazalevsky21@yandex.ru>
2021-01-08 11:04:24 +0700

    feat: Add HomeController with Views

--- SimpleSite/Controllers/HomeController.cs
+++ SimpleSite/Controllers/HomeController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SimpleSite.Domain;
 
 namespace SimpleSite.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DataManager dataManager;
+
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var pageIndexTextField = dataManager.TextFields.GetTextFieldByCodeWord("PageIndex");
+            return View(pageIndexTextField);
+        }
+
+        public IActionResult Contacts()
+        {
+            var pageContactsTextField = dataManager.TextFields.GetTextFieldByCodeWord("PageContacts");
+            return View(pageContactsTextField);
         }
     }
 }
